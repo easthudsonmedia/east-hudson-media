b841c086edc47c72b5b0b804c37cad51a09602f8
Chris Traver <cjt84@cornell.edu>
2016-06-29 21:43:28 -0700

    Intial commit

---
+++ js/youtube.js
@@ -0,0 +1,33 @@
+        $( ".end1" ).click(function() {
+            var video = $("#youtube1").attr("src");
+            $("#youtube1").attr("src","");
+            $("#youtube1").attr("src",video);
+        });
+        
+          $( ".end2" ).click(function() {
+            var video = $("#youtube2").attr("src");
+            $("#youtube2").attr("src","");
+            $("#youtube2").attr("src",video);
+        });
+          
+        $( ".end3" ).click(function() {
+            var video = $("#youtube3").attr("src");
+            $("#youtube3").attr("src","");
+            $("#youtube3").attr("src",video);
+        });
+    
+      $( ".end4" ).click(function() {
+            var video = $("#youtube4").attr("src");
+            $("#youtube4").attr("src","");
+            $("#youtube4").attr("src",video);
+        });
+        $( ".end5" ).click(function() {
+            var video = $("#youtube5").attr("src");
+            $("#youtube5").attr("src","");
+            $("#youtube5").attr("src",video);
+        });
+          $( ".end6" ).click(function() {
+            var video = $("#youtube6").attr("src");
+            $("#youtube6").attr("src","");
+            $("#youtube6").attr("src",video);
+        });
